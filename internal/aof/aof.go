e875ddcd4ddf668f7b2586c74099fe8ab6523aa2
t.rostomyan <roatomyan.tigran@rwb.ru>
2026-05-07 18:39:16 +0300

    codecrafters submit [skip ci]

--- internal/aof/aof.go
+++ internal/aof/aof.go
@@ -36,13 +36,12 @@ func New(ctx context.Context, cwd, aofDirname, aofFilename string, replay func(c
 	}
 	aof.root = root
 
-	data, err := os.ReadFile(filepath.Join(cwd, aofDirname, aofFilename+".manifest"))
-	if err != nil {
-		panic(err)
-	}
-	slog.Debug("manifest DEBUG", "data", string(data))
-
 	err = root.MkdirAll(aofDirname, 0o777)
+	if err != nil {
+		return nil, err
+	}
+
+	root, err = root.OpenRoot(aofDirname)
 	if err != nil {
 		return nil, err
 	}
